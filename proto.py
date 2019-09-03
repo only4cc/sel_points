834ed12829b03dae66b5d475a862078de9db6f15
only4cc@hotmail.com <only4cc@hotmail.com>
2019-09-02 21:50:08 -0400

    Graba csv con puntos seleccionados

--- proto.py
+++ proto.py
@@ -3,7 +3,7 @@
 
 # # Determinacion de puntos para toma de muestras
 
-# In[79]:
+# In[1431]:
 
 
 # python 3.7
@@ -11,42 +11,156 @@
 # Parametros de entrada:
 #    nombre del archivo formato csv
 #    numero minimo de puntos a seleccionar o ... porcentaje minimo de puntos a seleccionar
-#    distancia minima entre puntos
+#    distancia minima entre puntos <--- en mts
 #
 
 
-# In[104]:
+# In[1432]:
 
 
 import random
 import math
 
 
-# In[137]:
+# In[1433]:
 
 
-debug = True
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
 
 
-# In[141]:
+# In[1434]:
+
+
+import pandas as pd
+
+
+# In[1435]:
+
+
+# Print helpful messages
+# set to False in Production
+debug = False
+
+
+# In[1436]:
+
+
+# Lee el csv
+filename = '/Users/Julio/Dropbox7current/ATY/data/Puntos_Point.csv'
+filename = '../data/Puntos_Point.csv'
+
+name = 'A3-c'
+#name = 'A4-b'
+
+df = pd.read_csv(filename)
+df = df[(df['Name'] == name )]
+
+total_points =  len(df)
+
+# Prop. of total point number
+prop = 0.3
+# Minimal Distance Allowed
+min_distance_allowed = 141
+print('Total Points in csv:',total_points)
+print('% of Points to use:',prop*100, '| Number of points to use:',int(prop*len(df)) )
+print('Min. distance allowed:',min_distance_allowed)
+
+
+# In[1437]:
+
+
+df.head(3)
+
+
+# In[1438]:
+
+
+# Transform wkt_geom to X, Y coord.
+def regulariza(t):
+    nc  = t.replace('(','')
+    nc2 = nc.replace(')','')
+    return nc2 
+
+df[['sinuso','X','Y']] = df['wkt_geom'].str.split(" ",expand=True, )
+df.drop(columns=['sinuso'],inplace=True)
+df['X']=df['X'].apply(regulariza)
+df['Y']=df['Y'].apply(regulariza)
+df.head(3)
+
+
+# In[1439]:
+
+
+# To numeric
+df['X'] = pd.to_numeric(df['X'], errors='raise', downcast='float')
+df['Y'] = pd.to_numeric(df['Y'], errors='raise', downcast='float')
+
+
+# In[1440]:
+
+
+X = df['X'].values
+Y = df['Y'].values
+#df.info()
+
+# Minimos
+xmin = int(df['X'].min())
+ymin = int(df['Y'].min())
+
+# Maximos
+xmax = int(df['X'].max())
+ymax = int(df['Y'].max())
+
+print('Min. coord.:',df['X'].min(), df['Y'].min(),' | Integer:',xmin,ymin)
+print('Max. coord.:',df['X'].max(), df['Y'].max(),' | Integer:',xmax,ymax)
+
+
+# ### Puntos recibidos
+
+# In[1441]:
+
+
+plt.scatter(X,Y, marker='.', s = 5);
+
+
+# In[1442]:
 
 
 # universe_points se obtiene del archivo csv
 universe_points = []
-min_necesary_points = 15
+for i in range(0,len(X)):
+    x = X[i] #- xmin
+    y = Y[i] #- ymin
+    universe_points.append([x,y])
 
 
-# In[128]:
+# In[1443]:
 
 
+universe_points[0:4]
+
+
+# In[1444]:
+
+
+min_necesary_points = int(prop*len(universe_points))
+print('Min. number of Points needed:',min_necesary_points)
+
+
+# In[1445]:
+
+
+#universe_points
 # Crea un arreglo de points_number puntos  
 # Esto debe ser obtenido desde el archivo csv
-total_points_number = 100
-for x in range(total_points_number):
-  universe_points.append( [random.randint(1,100),random.randint(1,100)] ) 
+#total_points_number = 100
+#for x in range(total_points_number):
+#  universe_points.append( [random.randint(1,100),random.randint(1,100)] ) 
 
 
-# In[129]:
+# In[1446]:
 
 
 def distance(p,q):
@@ -56,25 +170,63 @@ def distance(p,q):
     return math.sqrt( (p[0]-q[0])*(p[0]-q[0]) + (p[1]-q[1])*(p[1]-q[1]) )  
 
 
-# In[130]:
+# In[ ]:
+
+
+
+
+
+# In[1447]:
 
 
 def get_point():
     '''
     Get a new point from the points array
     '''
-    i = random.randint(0,min_necesary_points-1)
+    i = random.randint(0, total_points-1)
     return universe_points[i]
 
 
-# In[139]:
+# In[1448]:
+
+
+def get_first_point():
+    '''
+    Get the first point from the points array
+    The nearest point to the centroid of all points
+    '''
+    xmin = 9999999999999999
+    xmax = -999999999999999
+    ymin = 9999999999999999
+    ymax = -999999999999999
+    first_point = []
+    for i in range(0,len(universe_points)):
+        x = universe_points[i][0]
+        y = universe_points[i][1]
+        if x < xmin: xmin = x
+        if x > xmax: xmax = x
+        if y < ymin: ymin = y
+        if y > ymax: ymax = y
+            
+    centroid = [ xmin + (xmax-xmin)/2 ,ymin + (ymax-ymin)/2 ]
+    if debug: print('*** Centroid:',centroid)
+    
+    d = 9999999999999999
+    for i in range(0,len(universe_points)):
+        if distance(universe_points[i], centroid) < d:
+            d = distance(universe_points[i], centroid)
+            first_point = universe_points[i]
+    
+    return first_point
+
+
+# In[1449]:
 
 
 def verify(point, selected_points):
     '''
     Verify that the new point is useful (based on a minimall distance)
     '''
-    min_distance_allowed = 7
     useful = False
     '''
     Compare the point with the rest of points selected
@@ -83,7 +235,7 @@ def verify(point, selected_points):
     while i < len(selected_points) :
         if debug: print('comparing ',point, selected_points[i], sep='')
         dist =  distance(point, selected_points[i])
-        if  dist <= min_distance_allowed and dist > 0:
+        if  dist <= min_distance_allowed:
             if debug: print('not ok',int(dist), ' < ',  min_distance_allowed)
             useful = False
             return useful
@@ -96,24 +248,29 @@ def verify(point, selected_points):
 
 # # Ejecucion de la seleccion de puntos
 
-# In[140]:
+# In[1450]:
 
 
-k = 0
-max_iter = 20
 selected_points=[]
 '''
 First point added to selected_points
 '''
-p = get_point()
+p = get_first_point()
 selected_points.append( p )
 if debug: print('first point',selected_points)
+
+
+# In[1451]:
+
+
+k = 0
+max_iter = 1000
 while ( len(selected_points) < min_necesary_points and k < max_iter ):
     p = get_point()
     if debug: print('checking if new point:',p, 'is useful ...')
     res = False
     res = verify(p, selected_points)
-    if res :
+    if res:
         if debug: print('yes, is useful')
         selected_points.append( p )
     else:
@@ -123,15 +280,67 @@ while ( len(selected_points) < min_necesary_points and k < max_iter ):
     #print('points selected', selected_points)
     
 
-print('\n*** end of selection.')
-print('*** number of selected points:',len(selected_points) )
+print('\n*** end of selection.\t',k, 'iterations')
+print('*** number of selected points:',len(selected_points))
 
 
-# In[135]:
+# In[1452]:
 
 
-print('selected points:')
-selected_points
+display = False
+if display:
+    print('selected points:')
+    selected_points
+
+
+# In[1453]:
+
+
+XX=[]
+YY=[]
+for p in selected_points:
+    XX.append(p[0]) 
+    YY.append(p[1]) 
+
+
+# ### Grafico con puntos seleccionados
+
+# In[1454]:
+
+
+plt.scatter(XX,YY, c='r', marker='+' );
+
+
+# In[ ]:
+
+
+plt.scatter(X,Y, c='b', marker='.', label='grid', s=10)
+plt.scatter(XX, YY, c='r', marker='s', label='selected', s=10)
+plt.legend(loc='lower left')
+plt.show()
+
+
+# ### Graba el csv con los puntos generados
+
+# In[1456]:
+
+
+index = []
+for i in range(0,len(XX)):
+    index.append(i)
+
+data = {
+    'x': XX,
+    'y': YY
+}
+dfsel = pd.DataFrame(data=data, columns=['x','y'], index=index)
+
+
+# In[1457]:
+
+
+filename_selected = '../data/sel_'+name+'.csv'
+dfsel.to_csv(filename_selected)
 
 
 # In[ ]:
